e7e3a1b87b5e60618551b14dd0dc94daed509ccb
Mark A Potts <Mark.Potts@noaa.gov>
2022-04-02 03:58:34 +0000

    update for build of intel_2020.2_mvapich2_g_develop with hash v8.3.0b12 on jet [ci skip]

--- develop/jet/intel/2020.2/g/mvapich2/2.3/out/build-intel_2020.2_mvapich2_g.bat
+++ develop/jet/intel/2020.2/g/mvapich2/2.3/out/build-intel_2020.2_mvapich2_g.bat
@@ -1,4 +1,4 @@
-Sat Apr 2 03:49:01 GMT 2022
+Sat Apr 2 03:58:34 GMT 2022
 #!/bin/sh -l
 #SBATCH --account=hfv3gfs
 #SBATCH -o build-intel_2020.2_mvapich2_g.bat_%j.o
--- develop/jet/intel/2020.2/g/mvapich2/2.3/out/test-intel_2020.2_mvapich2_g.bat
+++ develop/jet/intel/2020.2/g/mvapich2/2.3/out/test-intel_2020.2_mvapich2_g.bat
@@ -1,4 +1,4 @@
-Sat Apr 2 03:49:01 GMT 2022
+Sat Apr 2 03:58:33 GMT 2022
 #!/bin/sh -l
 #SBATCH --account=hfv3gfs
 #SBATCH -o test-intel_2020.2_mvapich2_g.bat_%j.o
