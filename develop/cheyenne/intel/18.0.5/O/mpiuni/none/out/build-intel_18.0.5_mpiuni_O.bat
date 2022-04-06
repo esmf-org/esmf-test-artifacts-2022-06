d271fed3e38cb4a45a5f202ded256d1f992a5205
Ryan Long <ryan.long@noaa.gov>
2022-04-06 07:31:51 -0600

    update for build of intel_18.0.5_mpiuni_O_develop with hash v8.3.0b12 on cheyenne [ci skip]

--- develop/cheyenne/intel/18.0.5/O/mpiuni/none/out/build-intel_18.0.5_mpiuni_O.bat
+++ develop/cheyenne/intel/18.0.5/O/mpiuni/none/out/build-intel_18.0.5_mpiuni_O.bat
@@ -1,4 +1,4 @@
-Wed Apr 6 06:28:40 MDT 2022
+Wed Apr 6 07:31:47 MDT 2022
 #!/bin/sh -l
 #PBS -N build-intel_18.0.5_mpiuni_O.bat
 #PBS -l walltime=1:00:00
--- develop/cheyenne/intel/18.0.5/O/mpiuni/none/out/test-intel_18.0.5_mpiuni_O.bat
+++ develop/cheyenne/intel/18.0.5/O/mpiuni/none/out/test-intel_18.0.5_mpiuni_O.bat
@@ -1,4 +1,4 @@
-Wed Apr 6 06:28:39 MDT 2022
+Wed Apr 6 07:31:47 MDT 2022
 #!/bin/sh -l
 #PBS -N test-intel_18.0.5_mpiuni_O.bat
 #PBS -l walltime=3:00:00
