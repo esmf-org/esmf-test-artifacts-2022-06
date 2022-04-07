2688f90fdfb3ac1fe4462f4d9f005ab1a244f644
Mark.Potts <Mark.Potts@noaa.gov>
2022-04-07 01:53:16 -0400

    update for test of intel_19.1.3_mpt_g_develop with hash v8.3.0b12 on discover [ci skip]

--- develop/discover/intel/19.1.3/g/mpt/2.17/out/test-intel_19.1.3_mpt_g.bat
+++ develop/discover/intel/19.1.3/g/mpt/2.17/out/test-intel_19.1.3_mpt_g.bat
@@ -1,4 +1,4 @@
-Thu Apr 7 01:13:30 EDT 2022
+Thu Apr 7 01:52:38 EDT 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-intel_19.1.3_mpt_g.bat_%j.o
--- develop/discover/pgi/20.4/g/mpiuni/none/out/build-pgi_20.4_mpiuni_g.bat
+++ develop/discover/pgi/20.4/g/mpiuni/none/out/build-pgi_20.4_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Wed Apr 6 03:01:52 EDT 2022
+Thu Apr 7 01:53:01 EDT 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o build-pgi_20.4_mpiuni_g.bat_%j.o
