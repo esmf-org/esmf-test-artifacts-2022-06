9b4686a980e9fcd542ca0f722bc06ff859cfe961
Daniel Rosen <drosen@ucar.edu>
2022-04-14 17:32:09 -0600

    update for build of gfortran_11.2.0_mpiuni_g_develop with hash v8.3.0b12-66-g9684790 on catania [ci skip]

---
+++ develop/catania/gfortran/11.2.0/g/mpiuni/none/out/build-gfortran_11.2.0_mpiuni_g.bat
@@ -0,0 +1,19 @@
+Thu Apr 14 17:32:09 MDT 2022
+#!/bin/bash -l
+export JOBID=12345
+
+module use /project/esmf/stack/modulefiles
+module load gcc/11.2.0  netcdf/4.7.4
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/Volumes/esmf/esmf-testing/gfortran_11.2.0_mpiuni_g_develop
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 4 2>&1| tee build_$JOBID.log
+
