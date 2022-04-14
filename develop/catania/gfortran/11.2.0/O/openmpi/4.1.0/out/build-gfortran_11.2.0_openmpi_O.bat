a21bfa731bee6d5ffefa9b9cc57817be03843218
Dan Rosen <drosen@cgdm-catania.cgd.ucar.edu>
2022-04-14 16:06:34 -0600

    update for build of gfortran_11.2.0_openmpi_O_develop with hash v8.3.0b12-65-g6d22e4b on catania [ci skip]

---
+++ develop/catania/gfortran/11.2.0/O/openmpi/4.1.0/out/build-gfortran_11.2.0_openmpi_O.bat
@@ -0,0 +1,19 @@
+Thu Apr 14 16:06:34 MDT 2022
+#!/bin/bash -l
+export JOBID=12345
+
+module use /project/esmf/stack/modulefiles
+module load gcc/11.2.0 openmpi/4.1.0 netcdf/4.7.4
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/Volumes/esmf/esmf-testing/gfortran_11.2.0_openmpi_O_develop
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=openmpi
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 4 2>&1| tee build_$JOBID.log
+
