64ac2131a464e23b6b372b25a29b4e0f13a6bb33
Rocky Dunlap <dunlap@ucar.edu>
2022-04-20 03:06:04 -0700

    update for build of intel_19.0.3_mpiuni_O_develop with hash v8.3.0b12-72-g1df70b4 on cori [ci skip]

--- develop/cori/intel/19.0.3/O/mpiuni/none/out/build-intel_19.0.3_mpiuni_O.bat
+++ develop/cori/intel/19.0.3/O/mpiuni/none/out/build-intel_19.0.3_mpiuni_O.bat
@@ -1,4 +1,4 @@
-Tue Apr 19 03:18:32 PDT 2022
+Wed Apr 20 03:06:01 PDT 2022
 #!/bin/sh -l
 #SBATCH --account=e3sm
 #SBATCH -o build-intel_19.0.3_mpiuni_O.bat_%j.o
--- develop/cori/intel/19.0.3/g/mpiuni/none/out/test-intel_19.0.3_mpiuni_g.bat
+++ develop/cori/intel/19.0.3/g/mpiuni/none/out/test-intel_19.0.3_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Tue Apr 19 03:18:20 PDT 2022
+Tue Apr 19 03:18:52 PDT 2022
 #!/bin/sh -l
 #SBATCH --account=e3sm
 #SBATCH -o test-intel_19.0.3_mpiuni_g.bat_%j.o
