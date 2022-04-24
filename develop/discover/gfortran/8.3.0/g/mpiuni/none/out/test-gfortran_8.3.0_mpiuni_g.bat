116f82b9cb307101225240b5366e0649550dcb43
Mark.Potts <Mark.Potts@noaa.gov>
2022-04-24 01:12:51 -0400

    update for build of gfortran_8.3.0_mpiuni_g_develop with hash v8.3.0b13 on discover [ci skip]

--- develop/discover/gfortran/8.3.0/g/mpiuni/none/out/test-gfortran_8.3.0_mpiuni_g.bat
+++ develop/discover/gfortran/8.3.0/g/mpiuni/none/out/test-gfortran_8.3.0_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Sat Apr 23 01:54:14 EDT 2022
+Sun Apr 24 01:12:48 EDT 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-gfortran_8.3.0_mpiuni_g.bat_%j.o
--- develop/discover/intel/19.1.3/g/intelmpi/19.1.3.304/out/build-intel_19.1.3_intelmpi_g.bat
+++ develop/discover/intel/19.1.3/g/intelmpi/19.1.3.304/out/build-intel_19.1.3_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Sat Apr 23 02:11:10 EDT 2022
+Sun Apr 24 01:12:45 EDT 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o build-intel_19.1.3_intelmpi_g.bat_%j.o
