318dc1787164458d9bce952b156ed8f8039508e7
Mark Potts <mpotts@redlineperf.com>
2022-04-24 02:28:59 -0500

    update for build of gfortran_8.3.0_mpiuni_O_develop with hash v8.3.0b13 on orion [ci skip]

--- develop/orion/gfortran/8.3.0/O/mpiuni/none/out/build-gfortran_8.3.0_mpiuni_O.bat
+++ develop/orion/gfortran/8.3.0/O/mpiuni/none/out/build-gfortran_8.3.0_mpiuni_O.bat
@@ -1,4 +1,4 @@
-Sat Apr 23 02:48:22 CDT 2022
+Sun Apr 24 02:28:13 CDT 2022
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o build-gfortran_8.3.0_mpiuni_O.bat_%j.o
--- develop/orion/gfortran/8.3.0/O/openmpi/4.0.2/out/build-gfortran_8.3.0_openmpi_O.bat
+++ develop/orion/gfortran/8.3.0/O/openmpi/4.0.2/out/build-gfortran_8.3.0_openmpi_O.bat
@@ -1,4 +1,4 @@
-Sat Apr 23 03:29:33 CDT 2022
+Sun Apr 24 02:28:40 CDT 2022
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o build-gfortran_8.3.0_openmpi_O.bat_%j.o
