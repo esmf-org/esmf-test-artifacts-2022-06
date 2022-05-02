455a551e869cce8e8b38877afd5c232efbffb434
Mark A Potts <Mark.Potts@noaa.gov>
2022-05-02 04:03:50 +0000

    update for build of intel_2020.2_intelmpi_g_develop with hash v8.3.0b13-3-g648c7f8 on jet [ci skip]

--- develop/jet/intel/2020.2/g/intelmpi/2020.2/out/build-intel_2020.2_intelmpi_g.bat
+++ develop/jet/intel/2020.2/g/intelmpi/2020.2/out/build-intel_2020.2_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Mon May 2 03:48:15 GMT 2022
+Mon May 2 04:03:50 GMT 2022
 #!/bin/sh -l
 #SBATCH --account=hfv3gfs
 #SBATCH -o build-intel_2020.2_intelmpi_g.bat_%j.o
--- develop/jet/intel/2020.2/g/intelmpi/2020.2/out/test-intel_2020.2_intelmpi_g.bat
+++ develop/jet/intel/2020.2/g/intelmpi/2020.2/out/test-intel_2020.2_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Mon May 2 03:48:15 GMT 2022
+Mon May 2 04:03:49 GMT 2022
 #!/bin/sh -l
 #SBATCH --account=hfv3gfs
 #SBATCH -o test-intel_2020.2_intelmpi_g.bat_%j.o
