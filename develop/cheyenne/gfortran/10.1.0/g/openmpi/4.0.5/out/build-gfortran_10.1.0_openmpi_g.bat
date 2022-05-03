041ecaa84343ca01813769aad4ce11a6e2002777
Ryan Long <ryan.long@noaa.gov>
2022-05-03 06:09:18 -0600

    update for build of gfortran_10.1.0_openmpi_g_develop with hash v8.3.0b13-7-g0b3ec3b on cheyenne [ci skip]

--- develop/cheyenne/gfortran/10.1.0/g/openmpi/4.0.5/out/build-gfortran_10.1.0_openmpi_g.bat
+++ develop/cheyenne/gfortran/10.1.0/g/openmpi/4.0.5/out/build-gfortran_10.1.0_openmpi_g.bat
@@ -1,4 +1,4 @@
-Mon May 2 06:54:10 MDT 2022
+Tue May 3 06:09:17 MDT 2022
 #!/bin/sh -l
 #PBS -N build-gfortran_10.1.0_openmpi_g.bat
 #PBS -l walltime=1:00:00
--- develop/cheyenne/gfortran/10.1.0/g/openmpi/4.0.5/out/test-gfortran_10.1.0_openmpi_g.bat
+++ develop/cheyenne/gfortran/10.1.0/g/openmpi/4.0.5/out/test-gfortran_10.1.0_openmpi_g.bat
@@ -1,4 +1,4 @@
-Mon May 2 06:54:10 MDT 2022
+Tue May 3 06:09:17 MDT 2022
 #!/bin/sh -l
 #PBS -N test-gfortran_10.1.0_openmpi_g.bat
 #PBS -l walltime=2:00:00
