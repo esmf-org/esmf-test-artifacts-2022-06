0b3567b0c97e630e5a86f2783fe861aa2244195c
Mark Potts <mpotts@redlineperf.com>
2022-05-04 03:39:54 -0500

    update for test of intel_2020.2_intelmpi_O_develop with hash v8.3.0b13-13-ge46b348 on orion [ci skip]

--- develop/orion/gfortran/8.3.0/g/openmpi/4.0.2/out/build-gfortran_8.3.0_openmpi_g.bat
+++ develop/orion/gfortran/8.3.0/g/openmpi/4.0.2/out/build-gfortran_8.3.0_openmpi_g.bat
@@ -1,4 +1,4 @@
-Wed May 4 02:35:41 CDT 2022
+Wed May 4 03:39:52 CDT 2022
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o build-gfortran_8.3.0_openmpi_g.bat_%j.o
--- develop/orion/intel/2020.2/O/intelmpi/2020.2/out/test-intel_2020.2_intelmpi_O.bat
+++ develop/orion/intel/2020.2/O/intelmpi/2020.2/out/test-intel_2020.2_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Wed May 4 02:37:20 CDT 2022
+Wed May 4 03:39:31 CDT 2022
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o test-intel_2020.2_intelmpi_O.bat_%j.o
