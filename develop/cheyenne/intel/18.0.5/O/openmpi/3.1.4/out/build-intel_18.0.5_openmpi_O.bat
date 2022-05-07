ceaee2b91125546d84f4fe954833f1e2023b1fcf
Ryan Long <ryan.long@noaa.gov>
2022-05-07 17:11:28 -0600

    update for build of intel_18.0.5_openmpi_O_develop with hash v8.3.0b14-2-gde129fc on cheyenne [ci skip]

--- develop/cheyenne/intel/18.0.5/O/openmpi/3.1.4/out/build-intel_18.0.5_openmpi_O.bat
+++ develop/cheyenne/intel/18.0.5/O/openmpi/3.1.4/out/build-intel_18.0.5_openmpi_O.bat
@@ -1,4 +1,4 @@
-Fri Mar 18 12:33:03 MDT 2022
+Sat May 7 17:11:28 MDT 2022
 #!/bin/sh -l
 #PBS -N build-intel_18.0.5_openmpi_O.bat
 #PBS -l walltime=1:00:00
@@ -9,6 +9,8 @@ Fri Mar 18 12:33:03 MDT 2022
 JOBID="`echo $PBS_JOBID | cut -d. -f1`"
 
 cd /glade/scratch/rlong/esmf-testing/intel_18.0.5_openmpi_O_develop
+
+module load python cmake
 module load intel/18.0.5 openmpi/3.1.4 netcdf/4.6.3
 module list >& module-build.log
 
