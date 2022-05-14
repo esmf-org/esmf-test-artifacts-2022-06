575d2f5febdde8cbbb6309d50c9bc0e7d7d67423
Ryan Long <ryan.long@noaa.gov>
2022-05-14 06:58:15 -0600

    update for build of gfortran_10.1.0_mpt_g_develop with hash v8.3.0b14-2-gde129fc on cheyenne [ci skip]

--- develop/cheyenne/gfortran/10.1.0/g/mpt/2.23/out/build-gfortran_10.1.0_mpt_g.bat
+++ develop/cheyenne/gfortran/10.1.0/g/mpt/2.23/out/build-gfortran_10.1.0_mpt_g.bat
@@ -1,4 +1,4 @@
-Sat May 14 06:12:59 MDT 2022
+Sat May 14 06:58:11 MDT 2022
 #!/bin/sh -l
 #PBS -N build-gfortran_10.1.0_mpt_g.bat
 #PBS -l walltime=1:00:00
--- develop/cheyenne/gfortran/10.1.0/g/mpt/2.23/out/test-gfortran_10.1.0_mpt_g.bat
+++ develop/cheyenne/gfortran/10.1.0/g/mpt/2.23/out/test-gfortran_10.1.0_mpt_g.bat
@@ -1,4 +1,4 @@
-Sat May 14 06:12:59 MDT 2022
+Sat May 14 06:58:11 MDT 2022
 #!/bin/sh -l
 #PBS -N test-gfortran_10.1.0_mpt_g.bat
 #PBS -l walltime=2:00:00
