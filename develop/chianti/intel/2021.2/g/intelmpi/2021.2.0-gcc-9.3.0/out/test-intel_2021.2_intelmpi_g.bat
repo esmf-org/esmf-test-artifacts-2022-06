4466b200442b18cf9382bdd73cc277fba4625a25
Mark Potts <Mark.Potts@noaa.gov>
2022-05-14 03:21:03 -0400

    update for test of intel_2021.2_intelmpi_g_develop with hash v8.3.0b14-2-gde129fc on chianti [ci skip]

--- develop/chianti/intel/2021.2/g/intelmpi/2021.2.0-gcc-9.3.0/out/test-intel_2021.2_intelmpi_g.bat
+++ develop/chianti/intel/2021.2/g/intelmpi/2021.2.0-gcc-9.3.0/out/test-intel_2021.2_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Sat May 14 02:56:54 EDT 2022
+Sat May 14 03:20:57 EDT 2022
 #!/bin/bash -l
 export JOBID=12346
 module load intel-oneapi-compilers/2021.2.0-gcc-9.3.0 intel-oneapi-mpi/2021.2.0-gcc-9.3.0 netcdf-c/4.8.0-intel-2021.2.0
