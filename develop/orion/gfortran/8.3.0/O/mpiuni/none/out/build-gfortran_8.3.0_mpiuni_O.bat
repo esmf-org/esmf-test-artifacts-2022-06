39e0aa4d5eaf44ce8f98705365c64f0b4095084e
Mark Potts <mpotts@redlineperf.com>
2022-05-20 02:36:01 -0500

    update for build of gfortran_8.3.0_openmpi_O_develop with hash v8.3.0b16 on orion [ci skip]

--- develop/orion/gfortran/8.3.0/O/mpiuni/none/out/build-gfortran_8.3.0_mpiuni_O.bat
+++ develop/orion/gfortran/8.3.0/O/mpiuni/none/out/build-gfortran_8.3.0_mpiuni_O.bat
@@ -1,8 +1,8 @@
 Fri May 20 02:35:59 CDT 2022
 #!/bin/sh -l
 #SBATCH --account=da-cpu
-#SBATCH -o build-gfortran_8.3.0_openmpi_O.bat_%j.o
-#SBATCH -e build-gfortran_8.3.0_openmpi_O.bat_%j.e
+#SBATCH -o build-gfortran_8.3.0_mpiuni_O.bat_%j.o
+#SBATCH -e build-gfortran_8.3.0_mpiuni_O.bat_%j.e
 #SBATCH --time=1:00:00
 #SBATCH --partition=orion
 #SBATCH --qos=batch
@@ -12,9 +12,8 @@ Fri May 20 02:35:59 CDT 2022
 export JOBID=$SLURM_JOBID
 
 module load intelpython3 cmake
-export ESMF_MPIRUN=mpirun.srun
-export ESMPY_MPIRUN=srun
-module load gcc/8.3.0 openmpi/4.0.2 netcdf/4.7.4
+export ESMF_MPIRUN=/work/noaa/da/mpotts/sandbox/gfortran_8.3.0_mpiuni_O_develop/src/Infrastructure/stubs/mpiuni/mpirun
+module load gcc/8.3.0  netcdf/4.7.4
 module list >& module-build.log
 
 set -x
@@ -22,9 +21,9 @@ export ESMF_NETCDF=nc-config
 
 export LD_PRELOAD=/apps/gcc-8/gcc-8.3.0/lib64/libstdc++.so
 tar xvfz ~/pytest-input.tar.gz
-export ESMF_DIR=/work/noaa/da/mpotts/sandbox/gfortran_8.3.0_openmpi_O_develop
+export ESMF_DIR=/work/noaa/da/mpotts/sandbox/gfortran_8.3.0_mpiuni_O_develop
 export ESMF_COMPILER=gfortran
-export ESMF_COMM=openmpi
+export ESMF_COMM=mpiuni
 export ESMF_BOPT='O'
 export ESMF_TESTEXHAUSTIVE='ON'
 export ESMF_TESTWITHTHREADS='ON'
