35eb1c4a64020b6e64f8237e336d3bdeaf90f80f
Role.Esmfmaint <esmfmaint@gmail.com>
2022-05-26 06:59:18 +0000

    update for test of gfortran_9.2.0b_openmpi_O_release_8.3.0 with hash v8.3.0b16-3-gc2b4738 on hera [ci skip]

--- develop/hera/gfortran/9.2.0b/O/openmpi/3.1.4/out/test-gfortran_9.2.0b_openmpi_O.bat
+++ develop/hera/gfortran/9.2.0b/O/openmpi/3.1.4/out/test-gfortran_9.2.0b_openmpi_O.bat
@@ -1,4 +1,4 @@
-Thu May 26 06:27:37 UTC 2022
+Thu May 26 06:59:16 UTC 2022
 #!/bin/sh -l
 #SBATCH --account=nems
 #SBATCH -o test-gfortran_9.2.0b_openmpi_O.bat_%j.o
--- release_8.3.0/hera/gfortran/9.2.0b/O/openmpi/3.1.4/out/build-gfortran_9.2.0b_openmpi_O.bat
+++ release_8.3.0/hera/gfortran/9.2.0b/O/openmpi/3.1.4/out/build-gfortran_9.2.0b_openmpi_O.bat
@@ -1,4 +1,4 @@
-Thu May 26 06:27:25 UTC 2022
+Thu May 26 06:59:01 UTC 2022
 #!/bin/sh -l
 #SBATCH --account=nems
 #SBATCH -o build-gfortran_9.2.0b_openmpi_O.bat_%j.o
