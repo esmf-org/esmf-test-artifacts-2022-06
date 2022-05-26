a31ac0ecd13f585aaa2d2cc2bebd6981ecff3de5
Bill Sacks <sacks@ucar.edu>
2022-05-26 17:48:29 -0600

    update for build of nvhpc_22.2_mpt_g_develop with hash v8.3.0b16 on cheyenne [ci skip]

--- develop/cheyenne/nvhpc/22.2/g/mpt/2.25/out/build-nvhpc_22.2_mpt_g.bat
+++ develop/cheyenne/nvhpc/22.2/g/mpt/2.25/out/build-nvhpc_22.2_mpt_g.bat
@@ -1,4 +1,4 @@
-Thu May 26 17:38:45 MDT 2022
+Thu May 26 17:48:28 MDT 2022
 #!/bin/sh -l
 #PBS -N build-nvhpc_22.2_mpt_g.bat
 #PBS -l walltime=1:00:00
@@ -9,6 +9,8 @@ Thu May 26 17:38:45 MDT 2022
 JOBID="`echo $PBS_JOBID | cut -d. -f1`"
 
 cd /glade/scratch/sacks/esmf-testing/nvhpc_22.2_mpt_g_develop
+
+module unload intel
 
 module load python cmake
 module load nvhpc/22.2 mpt/2.25 netcdf-mpi/4.8.1
