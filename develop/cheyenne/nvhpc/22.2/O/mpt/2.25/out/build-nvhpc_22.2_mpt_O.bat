6f20acba2259a79f3713729a10cf0503d23b95f1
Bill Sacks <sacks@ucar.edu>
2022-05-27 05:53:35 -0600

    update for test of nvhpc_22.2_mpt_O_develop with hash v8.3.0b16 on cheyenne [ci skip]

--- develop/cheyenne/nvhpc/22.2/O/mpt/2.25/out/build-nvhpc_22.2_mpt_O.bat
+++ develop/cheyenne/nvhpc/22.2/O/mpt/2.25/out/build-nvhpc_22.2_mpt_O.bat
@@ -1,4 +1,4 @@
-Fri May 27 04:38:55 MDT 2022
+Fri May 27 05:53:10 MDT 2022
 #!/bin/sh -l
 #PBS -N build-nvhpc_22.2_mpt_O.bat
 #PBS -l walltime=1:00:00
--- develop/cheyenne/nvhpc/22.2/O/mpt/2.25/out/test-nvhpc_22.2_mpt_O.bat
+++ develop/cheyenne/nvhpc/22.2/O/mpt/2.25/out/test-nvhpc_22.2_mpt_O.bat
@@ -1,4 +1,4 @@
-Fri May 27 04:38:55 MDT 2022
+Fri May 27 05:53:10 MDT 2022
 #!/bin/sh -l
 #PBS -N test-nvhpc_22.2_mpt_O.bat
 #PBS -l walltime=3:00:00
