05de15ca302c27bc54917a95da71077f7ff6388d
Rocky Dunlap <rocky.dunlap@noaa.gov>
2022-06-01 12:08:42 -0600

    update for build of gfortran_10.1.0_openmpi_O_develop with hash v8.4.0b00 on cheyenne [ci skip]

--- develop/cheyenne/gfortran/10.1.0/O/openmpi/4.0.5/out/test-gfortran_10.1.0_openmpi_O.bat
+++ develop/cheyenne/gfortran/10.1.0/O/openmpi/4.0.5/out/test-gfortran_10.1.0_openmpi_O.bat
@@ -1,4 +1,4 @@
-Thu May 26 06:53:44 MDT 2022
+Wed Jun 1 12:08:42 MDT 2022
 #!/bin/sh -l
 #PBS -N test-gfortran_10.1.0_openmpi_O.bat
 #PBS -l walltime=2:00:00
@@ -7,7 +7,7 @@ Thu May 26 06:53:44 MDT 2022
 #PBS -l select=1:ncpus=36:mpiprocs=36
 JOBID="`echo $PBS_JOBID | cut -d. -f1`"
 
-cd /glade/scratch/rlong/esmf-testing/gfortran_10.1.0_openmpi_O_develop
+cd /glade/scratch/dunlap/esmf-testing/gfortran_10.1.0_openmpi_O_develop
 
 module load cmake
 module load gnu/10.1.0 openmpi/4.0.5 netcdf/4.7.4
@@ -16,9 +16,8 @@ module list >& module-test.log
 set -x
 export ESMF_NETCDF=nc-config
 
-export ESMF_F90COMPILEOPTS="-fallow-argument-mismatch -fallow-invalid-boz"
 export ESMF_F90COMPILER=mpif90
-export ESMF_DIR=/glade/scratch/rlong/esmf-testing/gfortran_10.1.0_openmpi_O_develop
+export ESMF_DIR=/glade/scratch/dunlap/esmf-testing/gfortran_10.1.0_openmpi_O_develop
 export ESMF_COMPILER=gfortran
 export ESMF_COMM=openmpi
 export ESMF_BOPT='O'
