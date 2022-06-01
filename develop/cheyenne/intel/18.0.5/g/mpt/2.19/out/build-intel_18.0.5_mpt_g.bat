e6c5988fe0d15e857001b695cba76b0a0d9cdae1
Rocky Dunlap <rocky.dunlap@noaa.gov>
2022-06-01 12:51:58 -0600

    update for build of intel_18.0.5_mpt_g_develop with hash v8.4.0b00 on cheyenne [ci skip]

--- develop/cheyenne/intel/18.0.5/g/mpt/2.19/out/build-intel_18.0.5_mpt_g.bat
+++ develop/cheyenne/intel/18.0.5/g/mpt/2.19/out/build-intel_18.0.5_mpt_g.bat
@@ -1,4 +1,4 @@
-Thu May 26 08:19:56 MDT 2022
+Wed Jun 1 12:51:58 MDT 2022
 #!/bin/sh -l
 #PBS -N build-intel_18.0.5_mpt_g.bat
 #PBS -l walltime=1:00:00
@@ -8,7 +8,7 @@ Thu May 26 08:19:56 MDT 2022
 #PBS -l select=1:ncpus=36:mpiprocs=36
 JOBID="`echo $PBS_JOBID | cut -d. -f1`"
 
-cd /glade/scratch/rlong/esmf-testing/intel_18.0.5_mpt_g_develop
+cd /glade/scratch/dunlap/esmf-testing/intel_18.0.5_mpt_g_develop
 
 module load python cmake
 module load intel/18.0.5 mpt/2.19 netcdf/4.6.3
@@ -17,7 +17,7 @@ module list >& module-build.log
 set -x
 export ESMF_NETCDF=nc-config
 
-export ESMF_DIR=/glade/scratch/rlong/esmf-testing/intel_18.0.5_mpt_g_develop
+export ESMF_DIR=/glade/scratch/dunlap/esmf-testing/intel_18.0.5_mpt_g_develop
 export ESMF_COMPILER=intel
 export ESMF_COMM=mpt
 export ESMF_BOPT='g'
