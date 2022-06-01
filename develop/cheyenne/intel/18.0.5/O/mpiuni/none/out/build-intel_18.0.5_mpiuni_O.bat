127e3a5d611c115e5a63caa851c83d86ea34c798
Rocky Dunlap <rocky.dunlap@noaa.gov>
2022-06-01 12:32:37 -0600

    update for build of intel_18.0.5_mpiuni_O_develop with hash v8.4.0b00 on cheyenne [ci skip]

--- develop/cheyenne/intel/18.0.5/O/mpiuni/none/out/build-intel_18.0.5_mpiuni_O.bat
+++ develop/cheyenne/intel/18.0.5/O/mpiuni/none/out/build-intel_18.0.5_mpiuni_O.bat
@@ -1,4 +1,4 @@
-Thu May 26 07:40:27 MDT 2022
+Wed Jun 1 12:32:36 MDT 2022
 #!/bin/sh -l
 #PBS -N build-intel_18.0.5_mpiuni_O.bat
 #PBS -l walltime=1:00:00
@@ -8,17 +8,17 @@ Thu May 26 07:40:27 MDT 2022
 #PBS -l select=1:ncpus=36:mpiprocs=36
 JOBID="`echo $PBS_JOBID | cut -d. -f1`"
 
-cd /glade/scratch/rlong/esmf-testing/intel_18.0.5_mpiuni_O_develop
+cd /glade/scratch/dunlap/esmf-testing/intel_18.0.5_mpiuni_O_develop
 
 module load python cmake
-export ESMF_MPIRUN=/glade/scratch/rlong/esmf-testing/intel_18.0.5_mpiuni_O_develop/src/Infrastructure/stubs/mpiuni/mpirun
+export ESMF_MPIRUN=/glade/scratch/dunlap/esmf-testing/intel_18.0.5_mpiuni_O_develop/src/Infrastructure/stubs/mpiuni/mpirun
 module load intel/18.0.5  netcdf/4.6.3
 module list >& module-build.log
 
 set -x
 export ESMF_NETCDF=nc-config
 
-export ESMF_DIR=/glade/scratch/rlong/esmf-testing/intel_18.0.5_mpiuni_O_develop
+export ESMF_DIR=/glade/scratch/dunlap/esmf-testing/intel_18.0.5_mpiuni_O_develop
 export ESMF_COMPILER=intel
 export ESMF_COMM=mpiuni
 export ESMF_BOPT='O'
