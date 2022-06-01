f2cf24eca38eab964058f385db9cfa1fbac7f8c5
Rocky Dunlap <rocky.dunlap@noaa.gov>
2022-06-01 12:03:47 -0600

    update for build of gfortran_9.1.0_mpt_O_develop with hash v8.4.0b00 on cheyenne [ci skip]

--- develop/cheyenne/gfortran/9.1.0/O/mpt/2.22/out/test-gfortran_9.1.0_mpt_O.bat
+++ develop/cheyenne/gfortran/9.1.0/O/mpt/2.22/out/test-gfortran_9.1.0_mpt_O.bat
@@ -1,4 +1,4 @@
-Thu May 26 06:44:07 MDT 2022
+Wed Jun 1 12:03:46 MDT 2022
 #!/bin/sh -l
 #PBS -N test-gfortran_9.1.0_mpt_O.bat
 #PBS -l walltime=2:00:00
@@ -7,7 +7,7 @@ Thu May 26 06:44:07 MDT 2022
 #PBS -l select=1:ncpus=36:mpiprocs=36
 JOBID="`echo $PBS_JOBID | cut -d. -f1`"
 
-cd /glade/scratch/rlong/esmf-testing/gfortran_9.1.0_mpt_O_develop
+cd /glade/scratch/dunlap/esmf-testing/gfortran_9.1.0_mpt_O_develop
 
 module load cmake
 module load gnu/9.1.0 mpt/2.22 netcdf/4.7.3
@@ -17,7 +17,7 @@ set -x
 export ESMF_NETCDF=nc-config
 
 export ESMF_F90COMPILER=mpif90
-export ESMF_DIR=/glade/scratch/rlong/esmf-testing/gfortran_9.1.0_mpt_O_develop
+export ESMF_DIR=/glade/scratch/dunlap/esmf-testing/gfortran_9.1.0_mpt_O_develop
 export ESMF_COMPILER=gfortran
 export ESMF_COMM=mpt
 export ESMF_BOPT='O'
