8b3ed9effbbc08fd92c17419927a0a1a4869cb35
Rocky Dunlap <rocky.dunlap@noaa.gov>
2022-06-01 12:47:32 -0600

    update for build of intel_18.0.5_openmpi_g_develop with hash v8.4.0b00 on cheyenne [ci skip]

--- develop/cheyenne/intel/18.0.5/g/openmpi/3.1.4/out/test-intel_18.0.5_openmpi_g.bat
+++ develop/cheyenne/intel/18.0.5/g/openmpi/3.1.4/out/test-intel_18.0.5_openmpi_g.bat
@@ -1,4 +1,4 @@
-Thu May 26 08:12:27 MDT 2022
+Wed Jun 1 12:47:31 MDT 2022
 #!/bin/sh -l
 #PBS -N test-intel_18.0.5_openmpi_g.bat
 #PBS -l walltime=3:00:00
@@ -7,7 +7,7 @@ Thu May 26 08:12:27 MDT 2022
 #PBS -l select=1:ncpus=36:mpiprocs=36
 JOBID="`echo $PBS_JOBID | cut -d. -f1`"
 
-cd /glade/scratch/rlong/esmf-testing/intel_18.0.5_openmpi_g_develop
+cd /glade/scratch/dunlap/esmf-testing/intel_18.0.5_openmpi_g_develop
 
 module load python cmake
 module load intel/18.0.5 openmpi/3.1.4 netcdf/4.6.3
@@ -16,7 +16,7 @@ module list >& module-test.log
 set -x
 export ESMF_NETCDF=nc-config
 
-export ESMF_DIR=/glade/scratch/rlong/esmf-testing/intel_18.0.5_openmpi_g_develop
+export ESMF_DIR=/glade/scratch/dunlap/esmf-testing/intel_18.0.5_openmpi_g_develop
 export ESMF_COMPILER=intel
 export ESMF_COMM=openmpi
 export ESMF_BOPT='g'
@@ -30,3 +30,12 @@ chmod +x runpython.sh
 cd nuopc-app-prototypes
 ./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
 
+
+cd ../src/addon/ESMPy
+
+export PATH=$PATH:$HOME/.local/bin
+python3 setup.py build 2>&1 | tee python_build.log
+ssh cheyenne6 /glade/scratch/dunlap/esmf-testing/intel_18.0.5_openmpi_g_develop/runpython.sh 2>&1 | tee python_build.log
+python3 setup.py test 2>&1 | tee python_test.log
+python3 setup.py test_examples 2>&1 | tee python_examples.log
+python3 setup.py test_regrid_from_file 2>&1 | tee python_regrid.log
