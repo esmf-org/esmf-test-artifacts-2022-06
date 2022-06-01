e1998661782c82bc15dfee2461e0a0102f552bd0
Rocky Dunlap <rocky.dunlap@noaa.gov>
2022-06-01 12:14:44 -0600

    update for build of gfortran_7.4.0_openmpi_g_develop with hash v8.4.0b00 on cheyenne [ci skip]

--- develop/cheyenne/gfortran/7.4.0/g/openmpi/4.0.3/out/test-gfortran_7.4.0_openmpi_g.bat
+++ develop/cheyenne/gfortran/7.4.0/g/openmpi/4.0.3/out/test-gfortran_7.4.0_openmpi_g.bat
@@ -1,4 +1,4 @@
-Thu May 26 06:57:14 MDT 2022
+Wed Jun 1 12:14:43 MDT 2022
 #!/bin/sh -l
 #PBS -N test-gfortran_7.4.0_openmpi_g.bat
 #PBS -l walltime=2:00:00
@@ -7,7 +7,7 @@ Thu May 26 06:57:14 MDT 2022
 #PBS -l select=1:ncpus=36:mpiprocs=36
 JOBID="`echo $PBS_JOBID | cut -d. -f1`"
 
-cd /glade/scratch/rlong/esmf-testing/gfortran_7.4.0_openmpi_g_develop
+cd /glade/scratch/dunlap/esmf-testing/gfortran_7.4.0_openmpi_g_develop
 
 module load cmake
 module load gnu/7.4.0 openmpi/4.0.3 netcdf/4.7.3
@@ -17,7 +17,7 @@ set -x
 export ESMF_NETCDF=nc-config
 
 export ESMF_F90COMPILER=mpif90
-export ESMF_DIR=/glade/scratch/rlong/esmf-testing/gfortran_7.4.0_openmpi_g_develop
+export ESMF_DIR=/glade/scratch/dunlap/esmf-testing/gfortran_7.4.0_openmpi_g_develop
 export ESMF_COMPILER=gfortran
 export ESMF_COMM=openmpi
 export ESMF_BOPT='g'
