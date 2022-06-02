917fed6ce8822709dc1febab698919dc21cbf1d3
Role.Esmfmaint <esmfmaint@gmail.com>
2022-06-02 06:02:12 +0000

    update for build of gfortran_9.2.0a_mpiuni_g_develop with hash v8.4.0b00 on hera [ci skip]

--- develop/hera/gfortran/9.2.0a/g/mpiuni/none/out/build-gfortran_9.2.0a_mpiuni_g.bat
+++ develop/hera/gfortran/9.2.0a/g/mpiuni/none/out/build-gfortran_9.2.0a_mpiuni_g.bat
@@ -1,9 +1,9 @@
-Wed Jun 1 06:43:27 UTC 2022
+Thu Jun 2 06:02:10 UTC 2022
 #!/bin/sh -l
 #SBATCH --account=nems
 #SBATCH -o build-gfortran_9.2.0a_mpiuni_g.bat_%j.o
 #SBATCH -e build-gfortran_9.2.0a_mpiuni_g.bat_%j.e
-#SBATCH --time=1:00:00
+#SBATCH --time=2:00:00
 #SBATCH --partition=hera
 #SBATCH --qos=batch
 #SBATCH --nodes=1
@@ -12,6 +12,7 @@ Wed Jun 1 06:43:27 UTC 2022
 export JOBID=$SLURM_JOBID
 
 module load cmake
+export ESMF_MPIRUN=/scratch1/NCEPDEV/stmp2/role.esmfmaint/gfortran_9.2.0a_mpiuni_g_develop/src/Infrastructure/stubs/mpiuni/mpirun
 module load gnu/9.2.0  netcdf/4.7.2
 module list >& module-build.log
 
