4d27eea0c45b23ec1ce51a5be0cdb066a45f13bd
Mark.Potts <Mark.Potts@noaa.gov>
2022-06-03 02:13:16 -0400

    update for build of pgi_20.4_mpiuni_O_develop with hash v8.4.0b00 on discover [ci skip]

--- develop/discover/pgi/20.4/O/mpiuni/none/out/test-pgi_20.4_mpiuni_O.bat
+++ develop/discover/pgi/20.4/O/mpiuni/none/out/test-pgi_20.4_mpiuni_O.bat
@@ -1,4 +1,4 @@
-Thu Jun 2 02:57:56 EDT 2022
+Fri Jun 3 02:12:47 EDT 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-pgi_20.4_mpiuni_O.bat_%j.o
--- release_8.3.0/discover/gfortran/10.1.0/g/intelmpi/19.1.3.304/out/build-gfortran_10.1.0_intelmpi_g.bat
+++ release_8.3.0/discover/gfortran/10.1.0/g/intelmpi/19.1.3.304/out/build-gfortran_10.1.0_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Thu Jun 2 02:14:05 EDT 2022
+Fri Jun 3 02:13:02 EDT 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o build-gfortran_10.1.0_intelmpi_g.bat_%j.o
