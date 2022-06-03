4f9066b923faa13d7d7233991e89735a61516626
Rocky Dunlap <dunlap@ucar.edu>
2022-06-03 04:43:16 -0700

    update for test of intel_19.1.2_mpi_O_develop with hash v8.4.0b00 on cori [ci skip]

--- develop/cori/intel/19.1.2/O/mpi/7.7.19/out/test-intel_19.1.2_mpi_O.bat
+++ develop/cori/intel/19.1.2/O/mpi/7.7.19/out/test-intel_19.1.2_mpi_O.bat
@@ -1,4 +1,4 @@
-Fri Jun 3 02:38:44 PDT 2022
+Fri Jun 3 04:42:29 PDT 2022
 #!/bin/sh -l
 #SBATCH --account=e3sm
 #SBATCH -o test-intel_19.1.2_mpi_O.bat_%j.o
--- release_8.3.0/cori/intel/19.1.2/O/mpi/7.7.19/out/build-intel_19.1.2_mpi_O.bat
+++ release_8.3.0/cori/intel/19.1.2/O/mpi/7.7.19/out/build-intel_19.1.2_mpi_O.bat
@@ -1,4 +1,4 @@
-Fri Jun 3 02:35:56 PDT 2022
+Fri Jun 3 04:42:40 PDT 2022
 #!/bin/sh -l
 #SBATCH --account=e3sm
 #SBATCH -o build-intel_19.1.2_mpi_O.bat_%j.o
