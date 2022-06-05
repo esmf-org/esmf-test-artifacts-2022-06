72ce5dd1d51081df036e8367908127c7e58bdcfb
Rocky Dunlap <rocky.dunlap@noaa.gov>
2022-06-05 03:09:09 -0600

    update for build of intel_18.0.5_mpiuni_O_develop with hash v8.4.0b01 on cheyenne [ci skip]

--- develop/cheyenne/intel/18.0.5/O/mpiuni/none/out/build-intel_18.0.5_mpiuni_O.bat
+++ develop/cheyenne/intel/18.0.5/O/mpiuni/none/out/build-intel_18.0.5_mpiuni_O.bat
@@ -1,4 +1,4 @@
-Sat Jun 4 03:08:46 MDT 2022
+Sun Jun 5 03:09:07 MDT 2022
 #!/bin/sh -l
 #PBS -N build-intel_18.0.5_mpiuni_O.bat
 #PBS -l walltime=1:00:00
--- develop/cheyenne/intel/18.0.5/O/mpiuni/none/out/test-intel_18.0.5_mpiuni_O.bat
+++ develop/cheyenne/intel/18.0.5/O/mpiuni/none/out/test-intel_18.0.5_mpiuni_O.bat
@@ -1,4 +1,4 @@
-Sat Jun 4 03:08:46 MDT 2022
+Sun Jun 5 03:09:07 MDT 2022
 #!/bin/sh -l
 #PBS -N test-intel_18.0.5_mpiuni_O.bat
 #PBS -l walltime=3:00:00
