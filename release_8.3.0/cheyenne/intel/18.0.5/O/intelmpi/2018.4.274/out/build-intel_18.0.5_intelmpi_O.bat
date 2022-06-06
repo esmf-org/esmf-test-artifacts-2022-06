0017edd2b68639039ef3654ec69c891a538943e1
Rocky Dunlap <rocky.dunlap@noaa.gov>
2022-06-06 12:58:13 -0600

    update for build of intel_18.0.5_intelmpi_O_release_8.3.0 with hash v8.3.0b17-2-g86d44b3 on cheyenne [ci skip]

--- release_8.3.0/cheyenne/intel/18.0.5/O/intelmpi/2018.4.274/out/build-intel_18.0.5_intelmpi_O.bat
+++ release_8.3.0/cheyenne/intel/18.0.5/O/intelmpi/2018.4.274/out/build-intel_18.0.5_intelmpi_O.bat
@@ -1,7 +1,7 @@
-Mon Jun 6 03:13:36 MDT 2022
+Mon Jun 6 12:58:12 MDT 2022
 #!/bin/sh -l
 #PBS -N build-intel_18.0.5_intelmpi_O.bat
-#PBS -l walltime=1:00:00
+#PBS -l walltime=2:00:00
 #PBS -l walltime=3:00:00
 #PBS -q regular
 #PBS -A p93300606
